d9c196a967140b97fcbe7251966cde2e581e05ef
SaurashisBose24 <saurashisbose@gmail.com>
2024-09-20 20:01:33 +0530

    css added

--- src/components/Desktop2.jsx
+++ src/components/Desktop2.jsx
@@ -7,25 +7,25 @@ import './Desktop2.css'
 const Desktop2 = () => {
   return (
     <>
-        <div className='flex flex-col justify-center items-center'>
+        <div className='TopClass'>
             <div className='texts'>Sentiment Analysis</div>
             <div className='texts'>Decode Emotions In Texts</div>
-            <div><img width={360} height={159} className='top-0.5 bottom-0.5' src={image} alt="emoji" /></div>
+            <div><img width={360} height={159}  src={image} alt="emoji" /></div>
         </div>
 
         <div className='container'>
             <div className='input gridTexts'>
                 <div>Input</div>
-                <input type="text" className='rounded-md bg-slate-300 w-40 h-5 ' />
+                <input type="text" className='inputClass2' />
             </div>
             <div className='output gridTexts'>
                 <div>Output</div>
-                <input type="text" className='rounded-md bg-slate-300 w-40 h-5' />
+                <input type="text" className='inputClass2' />
             </div>
             <img className='picture' src={image2} alt="" />
             <div className='inputcsv gridTexts'>
                 <div>Input CSV</div>
-                <input type="file" className='rounded-md bg-slate-300 w-70 text-base font-serif ' />
+                <input type="file" className=' inputCSV2 '/>
             </div>
             <div className='graph gridTexts'>
                 <div>Graph</div>
